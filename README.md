8f0376f38b12b41e0e023e0722345356d3a46566
entbappy <entbappy73@gmail.com>
2024-02-20 10:31:31 +0600

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # End-to-end-NLP-Project-Implementation
+
+
+## How to run?
+
+```bash
+conda create -n hate python=3.8 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="iNeuron",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
